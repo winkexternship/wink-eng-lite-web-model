2ed854f0d439d4f248d3b43535b7fce1184362ae
marchingoutlaw <angad.sawadh@gmail.com>
2022-03-01 12:04:38 +0530

    naive text summarizer

--- src/summary.js
+++ src/summary.js
@@ -6,17 +6,27 @@ var summary = function ( rdd, its ) {
     var numOfSentences = rdd.sentences.length;
     var aptTokens = [];
     var aptPOS = [ 'ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB' ];
+    var weights = [];
+    var convergenceThreshold =  0.0001;
+    var maxIteration = 100;
+    var dampingFactor = 0.85;
+    var numOfConvergence = 0;
+    var colSum = [];
 
-    // Getting appropriate tokens for text summarization
+    console.log(`Number of sentences in text:${numOfSentences}`);
+    // Getting appropriate tokens for text summarization (Preprocessing)
     for ( let i = 0; i < numOfSentences; i += 1) {
-        var temp = [];
+        const temp = [];
         for ( let j = sentences[i][0]; j <= sentences[i][1]; j += 1) {
-            if ( its.type( j, tokens, cache ) === 'word' && aptPOS.includes(its.pos(j, tokens, cache)) ) {
+            if ( its.type( j, tokens, cache ) === 'word' && !its.stopWordFlag( j, tokens, cache ) && aptPOS.includes(its.pos(j, tokens, cache)) ) {
                 temp.push(its.normal( j, tokens, cache ));
             }
         }
         aptTokens.push(temp);
     }
+    console.log(`Number of sentences in aptTokens:${aptTokens.length}`);
+    console.log('aptTokens:');
+    console.log(aptTokens);
 
     // Creating adjacency matrix for graph of sentences
     var senGraph = new Array(numOfSentences);
@@ -26,16 +36,56 @@ var summary = function ( rdd, its ) {
 
     // Populating the matrix with weights
     for ( let i = 0; i < numOfSentences - 1; i += 1) {
+        senGraph[i][i] = 0;
         for ( let j = i + 1; j < numOfSentences; j += 1) {
+            // change the following to implement different ways to calculate simmilarity value
             const numOfCommonTokens = aptTokens[i].filter((token) => aptTokens[j].includes(token)).length;
             senGraph[i][j] = numOfCommonTokens / (Math.log(aptTokens[i].length) + Math.log(aptTokens[j].length));
             senGraph[j][i] = senGraph[i][j];
         }
     }
+    console.log('SenGraph:');
+    console.log(senGraph);
 
-    // Ranking the sentences
+    // Normalizing the matrix
+    for ( let i = 0; i < numOfSentences; i += 1) {
+        colSum[i] = senGraph[i].reduce((previousValue, currentValue) =>  previousValue + currentValue ,0);
+    }
+    // for ( let i = 0; i < numOfSentences; i += 1) {
+    //     for ( let j = 0; j < numOfSentences; j += 1) {
+    //         senGraph[j][i] /= colSum[i];
+    //     }
+    // }
 
-    return 'This is a summary text for the given input text.';
+    // Initializing the weights vector
+    for ( let i = 0; i < numOfSentences; i += 1) {
+        weights.push({ idx: i, val: 1 / numOfSentences });
+    }
+    console.log('Intital Weights:');
+    console.log(weights);
+
+    // Ranking Algorithm (PageRank)
+    for ( let i = 0; i < maxIteration; i += 1) {
+        for ( let j = 0; j < numOfSentences; j += 1) {
+            let rank = 1 - dampingFactor;
+            rank += dampingFactor * senGraph[j].reduce((previousValue, currentValue, idx) => previousValue + ((currentValue / colSum[idx]) * weights[idx].val) ,0);
+            if ( Math.abs(weights[j] - rank) <= convergenceThreshold ) {
+                numOfConvergence += 1;
+            }
+            weights[j].val = rank;
+        }
+        if ( numOfConvergence === numOfSentences ) {
+            break;
+        }
+    }
+
+    // Sentence Selection
+    weights.sort( (a, b) => b.val - a.val );
+    console.log('Final Weights:');
+    console.log(weights);
+    return weights;
+
+    // return 'This is a summary text for the given input text.';
 };
 
 module.exports = summary;
