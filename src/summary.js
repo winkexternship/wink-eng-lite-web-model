fff6cb4f06a83b3510ae1a82f355f67e18d2d44e
marchingoutlaw <angad.sawadh@gmail.com>
2022-03-04 13:44:25 +0530

    final summarizer draft

--- src/summary.js
+++ src/summary.js
@@ -1,19 +1,20 @@
-var summary = function ( rdd, its ) {
+var summary = function ( rdd, its, as, simmilarity ) {
 
     var tokens = rdd.tokens;
     var sentences = rdd.sentences;
     var cache = rdd.cache;
     var numOfSentences = rdd.sentences.length;
     var aptTokens = [];
+    var bow = [];
     var aptPOS = [ 'ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB' ];
-    var weights = [];
     var convergenceThreshold =  0.0001;
     var maxIteration = 100;
     var dampingFactor = 0.85;
     var numOfConvergence = 0;
-    var colSum = [];
-    var nonZero = [];
-    var ranks = [];
+    var colSum = []; // Normalisation Parameter Method 2
+    var nonZero = []; // Normalisation Parameter Method 1
+    var ranks = []; // Used for pagerank without node weights
+    var weights = []; // Used for pagerank with node weights
 
     console.log(`Number of sentences in text:${numOfSentences}`);
     // Getting appropriate tokens for text summarization (Preprocessing)
@@ -24,6 +25,8 @@ var summary = function ( rdd, its ) {
                 temp.push(its.normal( j, tokens, cache ));
             }
         }
+        // bow will be used if we are using cosine simmilarity funtion
+        // bow.push(as.bow(temp));
         aptTokens.push(temp);
     }
     console.log(`Number of sentences in aptTokens:${aptTokens.length}`);
@@ -43,6 +46,8 @@ var summary = function ( rdd, its ) {
             // change the following to implement different ways to calculate simmilarity value
             const numOfCommonTokens = aptTokens[i].filter((token) => aptTokens[j].includes(token)).length;
             senGraph[i][j] = numOfCommonTokens / (Math.log(aptTokens[i].length) + Math.log(aptTokens[j].length));
+            // For cosine simmilarity
+            // senGraph[i][j] = simmilarity.bow.cosine(bow[i], bow[j]);
             if( Number.isNaN (senGraph[i][j] ) )
                 senGraph[i][j] = 0;
             senGraph[j][i] = senGraph[i][j];
@@ -51,44 +56,29 @@ var summary = function ( rdd, its ) {
     console.log('SenGraph:');
     console.log(senGraph);
 
-    // Page Rank
+    // Page Rank (Calculating Normalization Parameter Method 1) 
     for ( let i = 0; i < numOfSentences; i += 1) {
-        let c = 0;
-        for ( let j = 0; j < numOfSentences; j += 1 ) {
-            if ( senGraph[i][j] !== 0 ) c += 1;
-        }
-        nonZero.push( c );
+        nonZero[i] = senGraph[i].reduce((previousValue) =>  previousValue + 1 ,0);
     }
 
-    for ( let i = 0; i < numOfSentences; i += 1 ) {
-        let s = 0;
-        for ( let j = 0; j < numOfSentences; j += 1 ) {
-            if ( senGraph[i][j] !== 0)
-                s += senGraph[i][j] / nonZero[j];
-        }
-        ranks.push({ idx: i, val: s });
-        }
-
-
-
-    // // Normalizing the matrix
+    // Page Rank (Calculating Normalization Parameter Method 2)
     // for ( let i = 0; i < numOfSentences; i += 1) {
     //     colSum[i] = senGraph[i].reduce((previousValue, currentValue) =>  previousValue + currentValue ,0);
     // }
-    // // for ( let i = 0; i < numOfSentences; i += 1) {
-    // //     for ( let j = rdd, its.value, as.text, addons0; j < numOfSentences; j += 1) {
-    // //         senGraph[j][i] /= colSum[i];
-    // //     }
-    // // }
 
-    // // Initializing the weights vector
+    // Page Rank (Method 1 without Node Weights)
+    for ( let i = 0; i < numOfSentences; i += 1 ) {
+        const s = senGraph[i].reduce((previousValue, currentValue, idx) =>  previousValue + ( currentValue / nonZero[idx] )  ,0);
+        ranks.push({ idx: i, val: s });
+    }
+
+    // Page Rank (Method 2 with Node Weights)
+    // Initializing the weights vector (For PageRank Method 2)
     // for ( let i = 0; i < numOfSentences; i += 1) {
     //     weights.push({ idx: i, val: 1 / numOfSentences });
     // }
     // console.log('Intital Weights:');
     // console.log(weights);
-
-    // // Ranking Algorithm (PageRank)
     // for ( let i = 0; i < maxIteration; i += 1) {
     //     for ( let j = 0; j < numOfSentences; j += 1) {
     //         let rank = 1 - dampingFactor;
@@ -102,9 +92,10 @@ var summary = function ( rdd, its ) {
     //         break;
     //     }
     // }
-
+    
     // Sentence Selection
-    ranks.sort( (a, b) => b.val - a.val );
+    ranks.sort( (a, b) => b.val - a.val ); //For PageRank Method 1
+    // weights.sort( (a, b) => b.val - a.val ) //For Pagerank Method 2
     console.log('Final Weights:');
     console.log(ranks);
     
