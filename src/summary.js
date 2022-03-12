1201876513f8e903d90fbf56382c00feea8503b5
marchingoutlaw <angad.sawadh@gmail.com>
2022-03-13 02:05:56 +0530

    final testing model

--- src/summary.js
+++ src/summary.js
@@ -1,120 +1,246 @@
-var summary = function ( rdd, its, as, simmilarity, bm25 ) {
+let preprocessingNonBow = function ( rdd, its ) {
 
-    var tokens = rdd.tokens;
-    var sentences = rdd.sentences;
-    var cache = rdd.cache;
-    var numOfSentences = rdd.sentences.length;
-    var aptTokens = [];
-    var bow = [];
-    var aptPOS = [ 'ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB' ];
-    var convergenceThreshold =  0.0001;
-    var maxIteration = 100;
-    var dampingFactor = 0.85;
-    var numOfConvergence = 0;
-    var colSum = []; // Normalisation Parameter Method 2
-    var nonZero = []; // Normalisation Parameter Method 1
-    var ranks = []; // Used for pagerank without node weights
-    var weights = []; // Used for pagerank with node weights
+    // required information
+    const numOfSentences = rdd.sentences.length;
+    const sentences = rdd.sentences;
+    const tokens = rdd.tokens;
+    const cache = rdd.cache;
+    const aptTokens = [];
+    const paraStarts = [ 0 ];
+    const aptPOS =  [ 'ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB' ];
 
-    console.log(`Number of sentences in text:${numOfSentences}`);
-    // Getting appropriate tokens for text summarization (Preprocessing)
-    for ( let i = 0; i < numOfSentences; i += 1) {
-        const temp = [];
-        for ( let j = sentences[i][0]; j <= sentences[i][1]; j += 1) {
-            if ( its.type( j, tokens, cache ) === 'word' && !its.stopWordFlag( j, tokens, cache ) && aptPOS.includes(its.pos(j, tokens, cache)) ) {
-                temp.push(its.normal( j, tokens, cache ));
-            }
-        }
-        // bow will be used if we are using cosine simmilarity funtion
-        // bow.push(as.bow(temp));
-
-        // used for common terms
-        aptTokens.push(temp);
-    }
-
-    // bm25 learning and creating bow
-    aptTokens.forEach( colToken => bm25.learn(colToken) );
-    for ( let i=0; i<aptTokens.length; i+=1 ) {
-        bow.push(bm25.doc(i).out(its.bow));
-    }
-    console.log(bow);
-
-    console.log(`Number of sentences in aptTokens:${aptTokens.length}`);
-    console.log('aptTokens:');
-    console.log(aptTokens);
-
-    // Creating adjacency matrix for graph of sentences
-    var senGraph = new Array(numOfSentences);
-    for ( let i = 0; i < numOfSentences; i += 1) {
-        senGraph[i] = new Array(numOfSentences);
-    }
-
-    // Populating the matrix with weights
-    for ( let i = 0; i < numOfSentences; i += 1) {
-        senGraph[i][i] = 0;
-        for ( let j = i + 1; j < numOfSentences; j += 1) {
-            // change the following to implement different ways to calculate simmilarity value
-            const numOfCommonTokens = aptTokens[i].filter((token) => aptTokens[j].includes(token)).length;
-            senGraph[i][j] = numOfCommonTokens / (Math.log(aptTokens[i].length) + Math.log(aptTokens[j].length));
-            // For cosine simmilarity
-            // senGraph[i][j] = simmilarity.bow.cosine(bow[i], bow[j]);
-            if( !Number.isFinite (senGraph[i][j] )  )
-                senGraph[i][j] = 0;
-            senGraph[j][i] = senGraph[i][j];
-        }
-    }
-    console.log('SenGraph:');
-    console.log(senGraph);
-
-    // Page Rank (Calculating Normalization Parameter Method 1) 
-    for ( let i = 0; i < numOfSentences; i += 1) {
-        nonZero[i] = senGraph[i].reduce((previousValue) =>  previousValue + 1 ,0);
-    }
-
-    // Page Rank (Calculating Normalization Parameter Method 2)
-    // for ( let i = 0; i < numOfSentences; i += 1) {
-    //     colSum[i] = senGraph[i].reduce((previousValue, currentValue) =>  previousValue + currentValue ,0);
-    // }
-
-    // Page Rank (Method 1 without Node Weights)
+    // generation of aptTokens
+    let para = [];
     for ( let i = 0; i < numOfSentences; i += 1 ) {
-        const s = senGraph[i].reduce((previousValue, currentValue, idx) =>  previousValue + ( currentValue / nonZero[idx] )  ,0);
-        ranks.push({ idx: i, val: s });
+      const sen = [];
+      for ( let j = sentences[i][0]; j <= sentences[i][1]; j += 1 ) {
+        if (its.type( j, tokens, cache ) === 'tabCRLF' ) {
+          aptTokens.push(para);
+          paraStarts.push(i);
+          para = [];
+        }
+        if ( its.type( j, tokens, cache ) === 'word' && !its.stopWordFlag( j, tokens, cache ) && aptPOS.includes(its.pos(j, tokens, cache)) ) {
+          sen.push(its.normal( j, tokens, cache ));
+        }
+      }
+      para.push(sen);
+    }
+    aptTokens.push(para);
+    const textInfo = { aptTokens: aptTokens, paraStarts: paraStarts };
+
+    return textInfo;
+
+};
+
+
+let preprocessingBow = function ( rdd, its, as ) {
+
+    // required information
+    const numOfSentences = rdd.sentences.length;
+    const sentences = rdd.sentences;
+    const tokens = rdd.tokens;
+    const cache = rdd.cache;
+    const bow = [];
+    const paraStarts = [ 0 ];
+    const aptPOS =  [ 'ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB' ];
+
+    // generation of aptTokens
+    let para = [];
+    for ( let i = 0; i < numOfSentences; i += 1 ) {
+      const sen = [];
+      for ( let j = sentences[i][0]; j <= sentences[i][1]; j += 1 ) {
+        if (its.type( j, tokens, cache ) === 'tabCRLF' ) {
+          bow.push(para);
+          paraStarts.push(i);
+          para = [];
+        }
+        if ( its.type( j, tokens, cache ) === 'word' && !its.stopWordFlag( j, tokens, cache ) && aptPOS.includes(its.pos(j, tokens, cache)) ) {
+          sen.push(its.normal( j, tokens, cache ));
+        }
+      }
+      para.push(as.bow(sen));
+    }
+    bow.push(para);
+    const textInfo = { bow: bow, paraStarts: paraStarts };
+    return textInfo;
+
+};
+
+let bm25Bow = function ( aptTokens, bm25, its ) {
+
+    // variables
+    const numOfParagraphs = aptTokens.length;
+    const bow = [];
+
+    for ( let i = 0; i < numOfParagraphs; i += 1 ) {
+      aptTokens[i].forEach( (colToken) => bm25.learn(colToken) );
+      const para = [];
+      for ( let j = 0; j < aptTokens[i].length; i += 1 ) {
+        para.push(bm25.doc(j).out(its.bow));
+      }
+      bow.push(para);
+    }
+    return bow;
+
+};
+
+let createGraphCommonTokens = function ( para ) {
+
+    const numOfSentences = para.length;
+    const senGraph = new Array(numOfSentences);
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      senGraph[i] = new Array(numOfSentences);
     }
 
-    // Page Rank (Method 2 with Node Weights)
-    // Initializing the weights vector (For PageRank Method 2)
-    // for ( let i = 0; i < numOfSentences; i += 1) {
-    //     weights.push({ idx: i, val: 1 / numOfSentences });
-    // }
-    // console.log('Intital Weights:');
-    // console.log(weights);
-    // for ( let i = 0; i < maxIteration; i += 1) {
-    //     for ( let j = 0; j < numOfSentences; j += 1) {
-    //         let rank = 1 - dampingFactor;
-    //         rank += dampingFactor * senGraph[j].reduce((previousValue, currentValue, idx) => previousValue + ((currentValue / colSum[idx]) * weights[idx].val) ,0);
-    //         if ( Math.abs(weights[j] - rank) <= convergenceThreshold ) {
-    //             numOfConvergence += 1;
-    //         }
-    //         weights[j].val = rank;
-    //     }
-    //     if ( numOfConvergence === numOfSentences ) {
-    //         break;
-    //     }
-    // }
-    
-    // Sentence Selection
-    ranks.sort( (a, b) => b.val - a.val ); //For PageRank Method 1
-    // weights.sort( (a, b) => b.val - a.val ) //For Pagerank Method 2
-    console.log('Final Weights:');
-    console.log(ranks);
-    
-    return {
-        weights: ranks,
-        numOfSentences: numOfSentences
-    };
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      senGraph[i][i] = 0;
+        for ( let j = i + 1; j < numOfSentences; j += 1) {
+          const numOfCommonTokens = para[i].filter((token) => para[j].includes(token)).length;
+          senGraph[i][j] = numOfCommonTokens / (Math.log(para[i].length) + Math.log(para[j].length));
+          if ( !Number.isFinite(senGraph[i][j] )  )
+            senGraph[i][j] = 0;
+          senGraph[j][i] = senGraph[i][j];
+        }
+    }
+    return senGraph;
 
-    // return 'This is a summary text for the given input text.';
+};
+
+let createGraphCosine = function ( paraBow, simmilarity ) {
+
+    const numOfSentences = paraBow.length;
+    const senGraph = new Array(numOfSentences);
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      senGraph[i] = new Array(numOfSentences);
+    }
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      senGraph[i][i] = 0;
+        for ( let j = i + 1; j < numOfSentences; j += 1) {
+          senGraph[i][j] = simmilarity.bow.cosine(paraBow[i], paraBow[j]);
+          if ( !Number.isFinite(senGraph[i][j] )  )
+            senGraph[i][j] = 0;
+          senGraph[j][i] = senGraph[i][j];
+        }
+    }
+    return senGraph;
+
+};
+
+let pagerankWithoutWeights = function ( paraSenGraph ) {
+
+    const numOfSentences = paraSenGraph.length;
+    const nonZero = [];
+    const ranks = [];
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      nonZero[i] = paraSenGraph[i].reduce((previousValue) =>  previousValue + 1 ,0);
+    }
+
+    for ( let i = 0; i < numOfSentences; i += 1 ) {
+      const s = paraSenGraph[i].reduce((previousValue, currentValue, idx) =>  previousValue + ( currentValue / nonZero[idx] )  ,0);
+      ranks.push({ idx: i, val: s });
+    }
+    ranks.sort( (a, b) => b.val - a.val );
+    return ranks;
+
+};
+
+let pagerankWithWeights = function ( paraSenGraph ) {
+
+    const numOfSentences = paraSenGraph.length;
+    const colSum = [];
+    const weights = [];
+    const maxIteration = 100;
+    const dampingFactor = 0.85;
+    const convergenceThreshold =  0.0001;
+    let numOfConvergence = 0;
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      colSum[i] = paraSenGraph[i].reduce((previousValue, currentValue) =>  previousValue + currentValue ,0);
+    }
+
+    for ( let i = 0; i < numOfSentences; i += 1) {
+      weights.push({ idx: i, val: 1 / numOfSentences });
+    }
+
+    for ( let i = 0; i < maxIteration; i += 1) {
+      for ( let j = 0; j < numOfSentences; j += 1) {
+        let rank = 1 - dampingFactor;
+        rank += dampingFactor * paraSenGraph[j].reduce((previousValue, currentValue, idx) => previousValue + ((currentValue / colSum[idx]) * weights[idx].val) ,0);
+        if ( Math.abs(weights[j] - rank) <= convergenceThreshold ) {
+          numOfConvergence += 1;
+        }
+        weights[j].val = rank;
+      }
+      if ( numOfConvergence === numOfSentences ) {
+        break;
+      }
+    }
+    weights.sort( (a, b) => b.val - a.val );
+    return weights;
+};
+
+// Comment out blocks for which you want to run tests
+let summary = function ( rdd, its, as, simmilarity, bm25 ) {
+
+    const weights = [];
+    const summaryInfo = {};
+
+    // Common Intersection + Pagerank Without Weights
+    const textInfo = preprocessingNonBow( rdd, its);
+    for ( let i = 0; i < textInfo.aptTokens.length; i += 1 ) {
+        weights.push( pagerankWithoutWeights( createGraphCommonTokens( textInfo.aptTokens[i] ) ) );
+    }
+    summaryInfo.weights = weights;
+    summaryInfo.paraStarts = textInfo.paraStarts;
+
+    // // Cosine Simmilarity + Pagerank Without Weights
+    // const textInfo = preprocessingBow( rdd, its );
+    // for ( let i = 0; i < textInfo.bow.length; i += 1 ) {
+    //     weights.push( pagerankWithoutWeights( createGraphCosine( textInfo.bow[i], simmilarity ) ) );
+    // }
+    // summaryInfo.weights = weights;
+    // summaryInfo.paraStarts = textInfo.paraStarts;
+
+    // // BM25 + Pagerank Without Weights
+    // const textInfo = preprocessingNonBow( rdd, its);
+    // const bow = bm25Bow( textInfo.aptTokens, bm25, its );
+    // for ( let i = 0; i < aptTokens.length; i += 1 ) {
+    //     weights.push( pagerankWithoutWeights( createGraphCosine( bow[i], simmilarity ) ) );
+    // }
+    // summaryInfo.weights = weights;
+    // summaryInfo.paraStarts = textInfo.paraStarts;
+
+    // // Common Intersection + Pagerank With Weights
+    // const textInfo = preprocessingNonBow( rdd, its);
+    // for ( let i = 0; i < textInfo.aptTokens.length; i += 1 ) {
+    //     weights.push( pagerankWithWeights( createGraphCommonTokens( textInfo.aptTokens[i] ) ) );
+    // }
+    // summaryInfo.weights = weights;
+    // summaryInfo.paraStarts = textInfo.paraStarts;
+
+    // // Cosine Simmilarity + Pagerank With Weights
+    // const textInfo = preprocessingBow( rdd, its );
+    // for ( let i = 0; i < textInfo.bow.length; i += 1 ) {
+    //     weights.push( pagerankWithWeights( createGraphCosine( textInfo.bow[i], simmilarity ) ) );
+    // }
+    // summaryInfo.weights = weights;
+    // summaryInfo.paraStarts = textInfo.paraStarts;
+
+    // // BM25 + Pagerank With Weights
+    // const textInfo = preprocessingNonBow( rdd, its);
+    // const bow = bm25Bow( textInfo.aptTokens, bm25, its );
+    // for ( let i = 0; i < aptTokens.length; i += 1 ) {
+    //     weights.push( pagerankWithWeights( createGraphCosine( bow[i], simmilarity ) ) );
+    // }
+    // summaryInfo.weights = weights;
+    // summaryInfo.paraStarts = textInfo.paraStarts;
+
+    return summaryInfo;
+
 };
 
 module.exports = summary;
